5ee3099f434f36f4451c82983f565f4f1a0fb569
Roslairy <roslairy@crimro.me>
2015-09-09 12:50:42 +0800

    0.2.2
    bug fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,6 +30,6 @@ class Kernel extends ConsoleKernel {
 		$schedule->call ( function () {
 			$manager = new NoFManager;
 			$manager->run();
-		} )->cron ( '* * * * *' )->withoutOverlapping();
+		} )->cron ( '* * * * *' )->name('NoF')->withoutOverlapping();
 	}
 }
